e9a8911d4c3f8e3c2e3b4044bd75ce3cfa19ede0
Varun Arora <varun.arora@vanderbilt.edu>
2018-12-09 21:06:46 -0600

    add intro client code

---
+++ client/app.py
@@ -0,0 +1,20 @@
+import sys
+import zmq
+
+SERVER_IP = "ec2-54-164-205-229.compute-1.amazonaws.com"
+
+if len(sys.argv) < 1:
+    sys.exit("Must provide a username in order to connect to Messenger.")
+
+user_id = sys.argv[1]
+PORT = "5555"
+if len(sys.argv) > 2:
+    PORT = sys.argv[2]
+    int(PORT)
+
+# Socket to talk to server
+ctx = zmq.Context()
+socket = ctx.socket(zmq.SUB)
+
+print "Receiving messages from message server..."
+socket.connect("{}:{}".format(SERVER_IP, PORT))
