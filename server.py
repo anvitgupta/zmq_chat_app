a46949fef647c5706fce507673e7dc3e405333e4
anvitgupta <anvit.gupta@vanderbilt.edu>
2018-12-09 18:28:34 -0600

    added influx ips to server.py

--- server.py
+++ server.py
@@ -4,14 +4,21 @@ import json
 from influxdb.client import InfluxDBClientError
 from influxdb import client as influxdb
 
-influx_ip = ''
+influx_ip = ['ec2-52-23-246-145.compute-1.amazonaws.com', 
+            'ec2-52-90-226-153.compute-1.amazonaws.com',
+            'ec2-52-200-205-8.compute-1.amazonaws.com']
+
+# set up Influx database
+mydb1 = influxdb.InfluxDBClient(influx_ip[0], 8086, 'admin', 'admin', 'data')
+mydb2 = influxdb.InfluxDBClient(influx_ip[1], 8086, 'admin', 'admin', 'data') 
+mydb3 = influxdb.InfluxDBClient(influx_ip[2], 8086, 'admin', 'admin', 'data')  
+
+# create database
+# mydb1.create_database('data')            
 load_balancer_ip = ''
+round_robin = 0
 
 def main():
-    
-    # set up Influx database
-    mydb = influxdb.InfluxDBClient(influx_ip, 8086, 'admin', 'admin', 'data') 
-    mydb.create_database('data')
 
     # create sub socket and set up connection to pub 
     ctx = zmq.Context()
@@ -19,7 +26,7 @@ def main():
     socket.connect(load_balancer_ip)
     socket.setsockopt(zmq.SUBSCRIBE, '')
 
-    while True:
+    while round_robin < len(influx_ip):
         
         # receiver message and parse by topic
         message = socket.recv()
@@ -32,7 +39,15 @@ def main():
                     }
         }]
         
-        mydb.write_points(input_data)
+        if round_robin == 0:
+            mydb1.write_points(input_data)
+            round_robin = round_robin + 1
+        elif cnt == 1:
+            mydb2.write_points(input_data)
+            round_robin = round_robin + 1
+        else:
+            mydb3.write_points(input_data)
+            round_robin = 0
 
 if __name__ == "__main__":
     main() 
