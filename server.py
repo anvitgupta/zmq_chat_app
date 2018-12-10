9bc002807c47492909d37076354d1edda33d834f
anvitgupta <anvit.gupta@vanderbilt.edu>
2018-12-09 20:10:39 -0600

    added logic to server.py

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import sys
 import zmq
-import json
 from influxdb.client import InfluxDBClientError
 from influxdb import client as influxdb
 
@@ -9,45 +8,114 @@ influx_ip = ['ec2-52-23-246-145.compute-1.amazonaws.com',
             'ec2-52-200-205-8.compute-1.amazonaws.com']
 
 # set up Influx database
-mydb1 = influxdb.InfluxDBClient(influx_ip[0], 8086, 'admin', 'admin', 'data')
-mydb2 = influxdb.InfluxDBClient(influx_ip[1], 8086, 'admin', 'admin', 'data') 
-mydb3 = influxdb.InfluxDBClient(influx_ip[2], 8086, 'admin', 'admin', 'data')  
+mydb1 = influxdb.InfluxDBClient(influx_ip[0], 8086, 'admin', 'dubey', 'messenger')
+mydb2 = influxdb.InfluxDBClient(influx_ip[1], 8086, 'admin', 'dubey', 'messenger') 
+mydb3 = influxdb.InfluxDBClient(influx_ip[2], 8086, 'admin', 'dubey', 'messenger')  
 
-# create database
-# mydb1.create_database('data')            
-load_balancer_ip = ''
+context = zmq.Context() 
+socket_server = context.socket(zmq.REP)
+socket_server.bind('ec2-54-164-205-229.compute-1.amazonaws.com')
+
 round_robin = 0
 
 def main():
 
-    # create sub socket and set up connection to pub 
-    ctx = zmq.Context()
-    socket = ctx.socket(zmq.SUB)
-    socket.connect(load_balancer_ip)
-    socket.setsockopt(zmq.SUBSCRIBE, '')
-
     while round_robin < len(influx_ip):
         
-        # receiver message and parse by topic
-        message = socket.recv()
-        topic, sender, msg = message.split(" ")
+        message = socket_server.recv()
+        sender, receiver, msg, sg = message.split(" ")
 
-        input_data = [{
-                    'measurement': topic,
+        if sg == '0':
+            
+            sender_mailbox = [{
+                    'measurement': 'msgs',
                     'fields': {
-                        str(sender):msg
+                        'from': sender
+                        'mailbox': sender
+                        'msg': msg
+                        'chatname': receiver
                     }
-        }]
+            }]
+            
+            receiver_mailbox = [{
+                    'measurement': 'msgs',
+                    'fields': {
+                        'from': sender
+                        'mailbox': receiver
+                        'msg': msg
+                        'chatname': sender
+                    }
+            }]
+
+            if round_robin == 0:
+                mydb1.write_points(sender_mailbox)
+                mydb1.write_points(receiver_mailbox)
+                round_robin = round_robin + 1
+
+            elif cnt == 1:
+                mydb2.write_points(sender_mailbox)
+                mydb2.write_points(receiver_mailbox)
+                round_robin = round_robin + 1
+            else:
+                mydb3.write_points(sender_mailbox)
+                mydb3.write_points(receiver_mailbox)
+                round_robin = 0
         
-        if round_robin == 0:
-            mydb1.write_points(input_data)
-            round_robin = round_robin + 1
-        elif cnt == 1:
-            mydb2.write_points(input_data)
-            round_robin = round_robin + 1
-        else:
-            mydb3.write_points(input_data)
-            round_robin = 0
+        elif sg == '1':
+            
+            group = select(db: 'messenger')
+            .filter(exp:{'_measurement' == 'groups' and '_fields' == receiver})
+
+            members = group['members'].split(',')        
+
+            if round_robin == 0:
+                
+                mydb1.write_points(sender_mailbox)
+                
+                for i in members:
+                    mydb1.write_points([{
+                        'measurement': 'msgs',
+                        'fields': {
+                            'from': sender
+                            'mailbox': i
+                            'msg': msg
+                            'chatname': receiver
+                        }
+                    }])
+
+            elif cnt == 1:
+                
+                mydb2.write_points(sender_mailbox)
+                
+                for i in members:
+                    mydb2.write_points([{
+                        'measurement': 'msgs',
+                        'fields': {
+                            'from': sender
+                            'mailbox': i
+                            'msg': msg
+                            'chatname': receiver
+                        }
+                    }])
+
+                round_robin = round_robin + 1
+
+            else:
+                
+                mydb3.write_points(sender_mailbox)
+                
+                for i in members:
+                    mydb3.write_points([{
+                        'measurement': 'msgs',
+                        'fields': {
+                            'from': sender
+                            'mailbox': i
+                            'msg': msg
+                            'chatname': receiver
+                        }
+                    }])
+
+                round_robin = 0
 
 if __name__ == "__main__":
     main() 
