5b6b7ad62408453497166e1ebdac8b27117d2ed6
Suhaas Yerramreddy <ysuhaas@gmail.com>
2018-12-09 23:40:15 -0600

    Draft of finished server code

--- server.py
+++ server.py
@@ -17,18 +17,41 @@ mydb3 = influxdb.InfluxDBClient(
 
 context = zmq.Context()
 # The socket used to listen for incoming messages
-socket_server_sub = context.socket(zmq.SUB)
-socket_server_sub.bind('ec2-54-164-205-229.compute-1.amazonaws.com')
+reply_socket = context.socket(zmq.REP)
+socket_server_sub.bind('ec2-54-164-205-229.compute-1.amazonaws.com:9000')
+
+# The socket used to send out messages
+socket_server_pub = context.socket(zmq.PUB)
 
 
+def createUser(message, current_db):
+    # topic_name = message[7, :]
+    # topics.append(topic_name)
 
-def writeToDatabase(queue, message):
-    # Round robin select the next Influx instance to use
-    current_db = queue.popleft()
-    queue.append(current_db)
+
+def createGroup(message, current_db):
+    parts = message.split("|")
+    group_members = '|'.join(map(str, parts[2, :]))
+    new_group = [{
+            'measurement': 'groups',
+            'fields': {
+                'id': str(parts[1]),
+                'members': group_members,
+            }
+        }]
+    current_db.write_points(new_group)
+
+    for member in group_members:
+                if member != sender
+                    message = '|'.join(map(str, list(filter(lambda x: x != member, group)))) 
+                    topic = "/" + str(member)
+                    socket_server_pub.send("%d %d" % (topic, message))
+
+
+def writeToDatabase(message, current_db):
 
     # Parse the message
-    sender, receiver, msg, sg = message.split(" ")
+    action, sender, receiver, msg, sg = message.split(" ")
 
     # If this is a one-to-one message
     if sg == '0':
@@ -62,9 +85,9 @@ def writeToDatabase(queue, message):
     else:
 
         # Find all the members in this group
-        group = select('db': 'messenger').filter(exp: {'_measurement' == 'groups' and '_fields' == receiver})
-        members = group['members'].split(',')
-        
+        group = list(current_db.query("SELECT * from groups WHERE id='" + str(receiver) + "'").get_points())
+        members = group['members'].split('|')
+
         # Write this message in each of the members' mailboxes
         for i in members:
             current_db.write_points([{
@@ -76,24 +99,50 @@ def writeToDatabase(queue, message):
                     'chatname': receiver
                 }
             }])
+            
+            
+        
 
-def sendMessage(message):
-    pub_socket = context.socket(zmq.PUB)
-    sender, receiver, msg, sg = message.split(" ")
-    pub_socket.bind("\" + str(receiver))
-    pub_socket.send("%d %d" % (sender, message))
+
+def sendMessage(message, current_db):
+    parts = message.split("|")
+    topics = []
+    message = parts[3]
+    # If a single message
+    if message[:-1] == '0':
+        topics.append("/" + str(parts[2]))
+    # If a group message
+    else:
+        result_set = current_db.query('SELECT * from groups WHERE id=' + str(receiver))
+        # TODO: Do a DB lookup for members of group
+        for i in topics:
+            topics.append(i)
+        
+    for topic in topics:
+        pub_socket.send("%d %d" % (topic, message))
+
 
 def main():
+    print "Starting messaging server now..."
 
     # Create a queue of our Influx instances
     queue = deque([mydb1, mydb2, mydb3])
-
-    # Each time we recieve a message, write it to DB and send out to receivers
+    
     while True:
-        message = socket_server_sub.recv()
-        writeToDatabase(queue, messaage)
-        sendMessage(message)
+        # Round robin select the next Influx instance to use
+        current_db = queue.popleft()
+        queue.append(current_db)
 
+        message = reply_socket.recv()
+
+        if str(message[0:5]) == "CREATE":
+            createUser(message, current_db)
+        elif str(message[0:5]) == "MAKE":
+            createGroup(message, current_db)
+        else:
+            writeToDatabase(message, current_db)
+            sendMessage(message, current_db)
+        reply_socket.send("Message: " + message + "recieved.")
 
 if __name__ == "__main__":
     main()
